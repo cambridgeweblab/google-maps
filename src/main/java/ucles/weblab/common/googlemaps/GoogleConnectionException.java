57d6eed1257e37836c667388791db8d20e012f80
jwatson <watson.j@cambridgeassessment.org.uk>
2017-06-12 15:57:30 +0100

    WEM-1778: Changed the exception to be runtime - if the connection to google fails either the internet is broken, there's a major flaw in the code, it shouldn't really be expected

--- src/main/java/ucles/weblab/common/googlemaps/GoogleConnectionException.java
+++ src/main/java/ucles/weblab/common/googlemaps/GoogleConnectionException.java
@@ -3,7 +3,7 @@ package ucles.weblab.common.googlemaps;
 /**
  * Thrown when the server encounters an error connecting to the google maps services.
  */
-public class GoogleConnectionException extends Exception {
+public class GoogleConnectionException extends RuntimeException {
     public GoogleConnectionException(Throwable cause) {
         super(cause);
     }
